dbe9ea0319b44895ad198625ee87dc55ec6ed039
danlun2 <danlun2@APC105933.intra.vaxjo.se>
2016-02-22 15:06:32 +0100

    Start working with nu.t4.services.handledare

---
+++ src/java/nu/t4/services/handledare/HandledareService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.t4.services.handledare;
+
+/**
+ *
+ * @author danlun2
+ */
+public class HandledareService {
+    
+}
